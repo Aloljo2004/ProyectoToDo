23f294bf4e5d9f5a013c540fa27be21d873dd151
Aloljo2004 <aloljo@alumnatflorida.es>
2025-11-19 15:42:56 +0100

    Elimine el proyecto momentaneamente

--- supabase-todos - copia/src/services/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Variables de entorno
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-// Crear cliente
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
